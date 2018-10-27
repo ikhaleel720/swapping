42b03be013f92d64ef0068e3974b09eda9fda739
ikhaleel720 <ibrahimkhaleel720@gmail.com>
2018-10-27 14:48:43 +0530

    Addedlogic

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int x=10;
+	int y=5;
+	printf("values before swapping");
+	scanf("\n x: %d\n y:\d\n",x,y);
+
+	int temp=x;
+	x=y;
+	y=temp;
+	printf("\n values after swapping");
+	scanf("\n x:%d\n y:%d\n",x,y);
+}
+
