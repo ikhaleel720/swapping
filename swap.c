79321cfb4052cc440fe3293951fcf7ab5d788bab
ikhaleel720 <ibrahimkhaleel720@gmail.com>
2018-10-27 15:01:15 +0530

    create function

--- swap.c
+++ swap.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-void main()
+void swap (int *x,int *y)
+{
+	int temp= *x;
+	*x=*y;
+	*y=temp;
+}
+voi main()
 {
 	int x=10;
 	int y=5;
+
 	printf("values before swapping");
 	scanf("\n x: %d\n y:\d\n",x,y);
 
-	int temp=x;
-	x=y;
-	y=temp;
 	printf("\n values after swapping");
 	scanf("\n x:%d\n y:%d\n",x,y);
 }
